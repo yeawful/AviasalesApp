dc79c5d825e65ce3efb5594f9f5d8e540b6f6b77
Ilya Raketa <paketa1337@gmail.com>
2025-05-29 19:51:17 +0300

    add api, redux-thunk

--- src/components/TicketsList/index.tsx
+++ src/components/TicketsList/index.tsx
@@ -1,17 +1,40 @@
 import './index.scss';
 import Ticket from '../Ticket';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
+import { useEffect, useState } from 'react';
+import { fetchTickets } from '../../api/ticketApi';
 
 const TicketsList = () => {
+    const dispatch = useDispatch();
+    const { items: tickets } = useSelector((state: RootState) => state.tickets);
+    const [visibleTickets, setVisibleTickets] = useState(5);
+
+    useEffect(() => {
+        dispatch(fetchTickets() as any);
+    }, [dispatch]);
+
+    const loadMoreTickets = () => {
+        setVisibleTickets(prev => prev + 5);
+    };
+
     return (
         <div className="TicketsList">
-            <Ticket/>
-            <Ticket/>
-            <Ticket/>
-            <Ticket/>
-            <Ticket/>
-            <button className="ShowMoreTickets">
-                Показать еще 5 билетов!
-            </button>
+            {tickets.slice(0, visibleTickets).map((ticket, index) => (
+                <Ticket 
+                    key={`${ticket.price}-${ticket.carrier}-${ticket.segments[0].date}-${ticket.segments[1].date}-${index}`}
+                    ticket={ticket} 
+                />
+            ))}
+            
+            {visibleTickets < tickets.length && (
+                <button 
+                    className="ShowMoreTickets" 
+                    onClick={loadMoreTickets}
+                >
+                    Показать еще 5 билетов!
+                </button>
+            )}
         </div>
     );
 };
